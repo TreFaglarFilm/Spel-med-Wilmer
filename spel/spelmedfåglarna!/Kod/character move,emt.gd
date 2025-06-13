6ed7971b719c7912ffda3d68d4f23876aa410b88
TreFaglarFilm <edvin.carlsson@protonmail.com>
2025-06-14 00:09:02 +0200

    förberedde för att "flippa" ananas pojken

--- spel/spelmedfåglarna!/Kod/character move,emt.gd
+++ spel/spelmedfåglarna!/Kod/character move,emt.gd
@@ -29,6 +29,6 @@ func _physics_process(delta: float) -> void:
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("Walking")
+		_animated_sprite.play("Walking_right")
 	else:
 		_animated_sprite.play("idle")

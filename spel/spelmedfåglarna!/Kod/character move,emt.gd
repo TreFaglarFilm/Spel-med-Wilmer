8bf956ca9fe96efbe87ae4390ceab0e64bec4a30
TreFaglarFilm <edvin.carlsson@protonmail.com>
2025-06-14 12:14:37 +0200

    fixade hoppljudet
    du brickade hoppljudet... det är nu fixat.

--- spel/spelmedfåglarna!/Kod/character move,emt.gd
+++ spel/spelmedfåglarna!/Kod/character move,emt.gd
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		
 		velocity.y = JUMP_VELOCITY
-		$AudioStreamPlayer2D.play()
+		$"JumpSound".play()
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
